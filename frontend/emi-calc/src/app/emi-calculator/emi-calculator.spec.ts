623268022ea9b49e037845a894117cf3677a374e
Jay <142610767+JayD2003@users.noreply.github.com>
2025-12-05 17:16:02 +0530

    Fix typo in EmiCalculator import statement

--- frontend/emi-calc/src/app/emi-calculator/emi-calculator.spec.ts
+++ frontend/emi-calc/src/app/emi-calculator/emi-calculator.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { EmiCalulator } from './emi-calculator';
+import { EmiCalculator } from './emi-calculator';
 
 describe('EmiCalculator', () => {
   let component: EmiCalculator;
